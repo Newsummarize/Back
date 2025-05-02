d6a91d6024878966f0b79e4f770b4d235ae58ea6
hg2seo <hg2seo@naver.com>
2025-05-02 10:15:08 +0900

    fix: 프론트 포트 변경, 도메인 추가

--- src/main/java/com/newsummarize/backend/config/WebConfig.java
+++ src/main/java/com/newsummarize/backend/config/WebConfig.java
@@ -14,7 +14,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 API URL 허용
-                        .allowedOrigins("http://3.34.224.162:3000") // 허용할 프론트엔드 주소
+                        .allowedOrigins(
+                                "http://localhost:5173", // 로컬 개발용
+                                "https://newsummarize.com" // 실제 배포용 도메인
+                        )
+                        // 허용할 프론트엔드 주소
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true) // 쿠키/헤더 포함 허용 (JWT 인증할 때 필요할 수 있음)
