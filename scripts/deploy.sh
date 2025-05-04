d9daa301317b81bac7597ae71c67644d664cc062
soeusueumineu <songcho0000@naver.com>
2025-05-05 08:45:15 +0900

    Remove zip file from tracking and ignore

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,14 @@
 
 echo "🚀 [DEPLOY] 배포 스크립트 시작"
 
-cd /home/ubuntu
+# 기존 프로세스 종료
+echo "🛑 [DEPLOY] 기존 애플리케이션 종료 중..."
+pkill -f 'backend-0.0.1-SNAPSHOT.jar'
 
-JAR_NAME=backend-0.0.1-SNAPSHOT.jar
+# 새 애플리케이션 실행
+echo "✅ [DEPLOY] 새 JAR 실행 중:"
+nohup java -jar /home/ubuntu/backend-0.0.1-SNAPSHOT.jar > /home/ubuntu/nohup.out 2>&1 &
 
-echo "🛑 [DEPLOY] 기존 애플리케이션 종료 중... "
-PID=$(pgrep -f "$JAR_NAME")
-if [ -n "$PID" ]; then
-  echo "🔻 종료된 PID: $PID"
-  kill -15 "$PID"
-  sleep 5
-else
-  echo "🔹 실행 중인 프로세스 없음"
-fi
-
-echo "✅ [DEPLOY] 새 JAR 실행 중: $JAR_NAME"
-nohup java -jar "$JAR_NAME" > /home/ubuntu/nohup.out 2>&1 &
 echo "🎉 [DEPLOY] 배포 완료!"
+
+exit 0
