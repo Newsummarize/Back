09164959c6a85265d39175a80ff6d9c5fdfd11c6
hg2seo <hg2seo@naver.com>
2025-05-21 18:18:05 +0900

    refactor: 자동 생성 어노테이션 범위 축소, 미사용 import문 제거

--- src/main/java/com/newsummarize/backend/dto/ErrorResponse.java
+++ src/main/java/com/newsummarize/backend/dto/ErrorResponse.java
@@ -1,9 +1,9 @@
 package com.newsummarize.backend.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class ErrorResponse {
     private int status;
