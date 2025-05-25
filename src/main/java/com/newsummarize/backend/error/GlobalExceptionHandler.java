e3b2df839ce7ad109cbdad1cb61c26b6f5dfe074
hg2seo <hg2seo@naver.com>
2025-05-25 18:54:20 +0900

    refactor, style: Global Exception Handler 도입, Service Bean 설정, 기타 코드 스타일 수정

---
+++ src/main/java/com/newsummarize/backend/error/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.newsummarize.backend.error;
+
+import com.newsummarize.backend.error.exception.InternalFlaskErrorException;
+import com.newsummarize.backend.error.exception.InvalidRequestParameterException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(InvalidRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidRequestKeywordException(InvalidRequestParameterException e) {
+        System.out.println("[Log] >>> 검색어 또는 기간 값이 잘못된 요청");
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(400, HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+
+    @ExceptionHandler(InternalFlaskErrorException.class)
+    public ResponseEntity<ErrorResponse> handleInternalFlaskErrorException(InternalFlaskErrorException e) {
+        System.out.println("[Log] >>> 검색어 또는 기간 값이 잘못된 요청");
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(400, HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUndefinedException(Exception e) {
+        System.out.println("[Log] >>> 알 수 없는 오류.");
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(500, HttpStatus.INTERNAL_SERVER_ERROR, "예기치 못한 문제가 발생했습니다."));
+    }
+}
--- src/main/java/com/newsummarize/backend/service/SearchService.java
+++ src/main/java/com/newsummarize/backend/service/SearchService.java
@@ -2,6 +2,7 @@ package com.newsummarize.backend.service;
 
 import com.newsummarize.backend.domain.News;
 import com.newsummarize.backend.dto.NumericalTrendResponse;
+import com.newsummarize.backend.error.exception.InternalFlaskErrorException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -13,18 +14,15 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
-
-    private static final Set<String> VALID_PERIODS = Set.of("daily", "weekly", "monthly");
+    private final RestTemplate restTemplate;
 
     public List<News> getSearchedNews(String keyword) {
-        String flaskURL = "http://3.34.224.162:5006/search?keyword=" + keyword;
+        String flaskURL = "http://127.0.0.1:5006/search?keyword=" + keyword;
         try {
-            RestTemplate restTemplate = new RestTemplate();
             Map<String, Object> response = restTemplate.getForObject(flaskURL, Map.class);
             List<Map<String, Object>> articles = (List<Map<String, Object>>) response.get("articles");
 
@@ -46,45 +44,27 @@ public class SearchService {
     }
 
     public byte[] getKeywordTrendImage(String keyword, String period) {
-        if (!VALID_PERIODS.contains(period)) {
-            throw new IllegalArgumentException("기간(period)은 {daily, weekly, monthly} 중 하나여야 합니다.");
-        }
-        String flaskURL = "http://3.34.224.162:5006/search/analytics?keyword=" + keyword + "&period=" + period;
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<byte[]> response = restTemplate.getForEntity(flaskURL, byte[].class);
-            if (!response.getStatusCode().is2xxSuccessful()) {
-                throw new RuntimeException("내부 서버 호출 실패: " + response.getStatusCode());
-            }
-            return response.getBody();
-        } catch (Exception e) {
-            throw new RuntimeException("통신 중 에러 발생");
-        }
+        String flaskURL = "http://127.0.0.1:5006/search/analytics?keyword=" + keyword + "&period=" + period;
+        ResponseEntity<byte[]> response = restTemplate.getForEntity(flaskURL, byte[].class);
+
+        if (!response.getStatusCode().is2xxSuccessful())
+            throw new InternalFlaskErrorException("내부 서버 호출 실패: " + response.getStatusCode());
+
+        return response.getBody();
     }
 
     public NumericalTrendResponse getNumericalAnalyticData(String keyword, String period) {
+        String flaskURL = "http://127.0.0.1:5006/search/analytics_num?keyword=" + keyword + "&period=" + period;
+        ResponseEntity<NumericalTrendResponse> response = restTemplate.exchange(
+                flaskURL,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<NumericalTrendResponse>() {}
+        );
 
-        if (!VALID_PERIODS.contains(period)) {
-            throw new IllegalArgumentException("기간(period)은 {daily, weekly, monthly} 중 하나여야 합니다.");
-        }
+        if (!response.getStatusCode().is2xxSuccessful())
+            throw new InternalFlaskErrorException("내부 서버 호출 실패: " + response.getStatusCode());
 
-        String flaskURL = "http://localhost:5006/search/analytics_num?keyword=" + keyword + "&period=" + period;
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<NumericalTrendResponse> response = restTemplate.exchange(
-                    flaskURL,
-                    HttpMethod.GET,
-                    null,
-                    new ParameterizedTypeReference<NumericalTrendResponse>() {}
-            );
-
-            if (!response.getStatusCode().is2xxSuccessful()) {
-                throw new RuntimeException("내부 서버 호출 실패: " + response.getStatusCode());
-            }
-
-            return response.getBody();
-        } catch (Exception e) {
-            throw new RuntimeException("통신 중 에러 발생");
-        }
+        return response.getBody();
     }
 }
